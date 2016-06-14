9624e4fcbffd76d7e45c7ba07fb40d3ccd87ea32
Nothen <hardwick101@gmail.com>
2016-06-14 12:18:44 +0100

    Big changes, trying a bet bot

--- bot.py
+++ bot.py
@@ -3,42 +3,33 @@ import json
 import random
 
 
-def math(left: float, sign: str, right: float):
-    if sign == '+':
-        result = left + right
-    elif sign == '-':
-        result = left - right
-    elif sign == '/':
-        result = left / right
-    elif sign == '*':
-        result = left * right
-    return 'The answer is ' + str(result)
-
-
-def dndroll(dice: str):
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        return 'Format has to be in NdN!'
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    return result
-
-
-def roll(limit : int):
-    x = random.randint(1,limit)
-    return x
-
-
 with open('config.json') as file:
     config = json.load(file)
 
 description = "Please ignore this bot"
 client = discord.Client()
+pot = 0
+
+
+def placebet(points: int, outcome: str):
+    global pot
+    pot += points
+    print(pot)
+
+
+class PlaceBet():
+    def __init__(self, points, outcome):
+        self.points = points
+        self.outcome = outcome
 
 
 @client.event
 async def on_ready():
+    x = []
+    for member in client.get_all_members():
+        x.append(member.id)
+    print(x)
+
     print('Connected')
     print('Username: ' + client.user.name)
     print('ID: ' + client.user.id)
@@ -48,11 +39,29 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+
+    if message.content.startswith('!bet'):
+        try:
+            command, points, outcome = message.content.split()
+            if points.isdigit() and int(points) < 1000:
+                msg = outcome + " " + points
+                if outcome in ['win', 'lose']:
+                    placebet(int(points), outcome)
+                    msg = 'Bet placed for {}, the pot is now {}'.format(message.author.mention, pot)
+                else:
+                    msg = 'Please use the correct format\n!bet (AMOUNT) (OUTCOME)'
+            else:
+                msg = 'Please use the correct format\n!bet (AMOUNT) (OUTCOME)'
+        except ValueError:
+            return
+            msg = 'Please use the correct format\n!bet (AMOUNT) (Win or Lose)'
+
+        print(message.content)
+
+        await client.send_message(message.channel, msg)
+
     if message.content.startswith('!help'):
         await client.send_message(message.channel, "Insert some help here\nYeezy just jumped over jumpman")
-
-    if message.content.startswith('!join'):
-        await client.join_voice_channel(client.get_channel(id='191602778446102529'))
 
     if message.content.startswith('!okay'):
         await client.send_message(message.channel, 'okay okay okay okay okay OKAY https://streamable.com/om4j')
